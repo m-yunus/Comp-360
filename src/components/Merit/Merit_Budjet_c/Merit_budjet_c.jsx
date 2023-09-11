5588789d283bb6c99614f49097a61ff9ce173814
Akshay <akshaypanampatta8891@gmail.com>
2023-09-11 09:32:46 +0530

    merit-budjet-c completed

--- src/components/Merit/Merit_Budjet_c/Merit_budjet_c.jsx
+++ src/components/Merit/Merit_Budjet_c/Merit_budjet_c.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Merit_budjet_c.css";
 
-
 const Merit_budjet_c = () => {
   return (
     <>
@@ -24,10 +23,114 @@ const Merit_budjet_c = () => {
           </span>
         </div>
         <div className="budjet-c">
+          <p className=" flex justify-end">8%, $367,000</p>
+          <div className="flex justify-end">
+            <table className="budjet-table-a">
+              <tbody>
+                <tr>
+                  <td colSpan="3">Parity (Comp-Ratio or RP)</td>
+                </tr>
+                <tr>
+                  <td>Low to 80</td>
+                  <td>80 to 120</td>
+                  <td>120 & more</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="flex gap-5 mt-5 justify-end">
+            <table className="budjet-table-b">
+              <tr>
+                <td className="center-text">Performance</td>
+                <td>
+                  <div className="rows">3</div>
+                  <div className="rows">2</div>
+                  <div className="">1</div>
+                </td>
+              </tr>
+            </table>
+            <table className="budjet-table-c">
+              <tr>
+                <td>20%</td>
+                <td>15%</td>
+                <td>10%</td>
+              </tr>
+              <tr>
+                <td>12%</td>
+                <td>10%</td>
+                <td>8%</td>
+              </tr>
+              <tr>
+                <td>7%</td>
+                <td>5%</td>
+                <td>2%</td>
+              </tr>
+            </table>
+          </div>
+          <div className="merit-bdjy-c-btn flex gap-20">
+            <button className="bck-btn">Cancel</button>
+            <button className="cnt-btn">Calculate</button>
+          </div>
+        </div>
+        <div className="merit-budjet">
+          <div className="flex ">
+            <h2>Do you have a budget for covering pay gap?</h2>
+            <div className="flex gap-8 ms-32">
+              <span>
+                <input type="radio" name="" id="" />
+                Yes
+              </span>
+              <span className="ms-2">
+                {" "}
+                <input type="radio" name="" id="" />
+                No
+              </span>
+            </div>
+          </div>
 
+          <div className="flex gap-8 my-4 items-center ms-20">
+            <h2>Pay gap budget</h2>
+            <input className="ms-[102px]" placeholder="Enter" type="text" />
+            <span>
+              <input type="radio" name="" id="" />%
+            </span>
+            <span className="ms-5">
+              {" "}
+              <input type="radio" name="" id="" />$
+            </span>
+          </div>
+          <div className="flex gap-10">
+            <h2>Do you want to keep a buffer?</h2>
+            <span className="flex items-center ms-[187px]">
+              <input type="radio" name="" id="" />
+              Yes
+            </span>
+            <span className="flex items-center">
+              {" "}
+              <input type="radio" name="" id="" />
+              No
+            </span>
+          </div>
+          <div className="flex gap-8 my-4 items-center ms-20">
+            <h2>Buffer required</h2>
+            <input placeholder="Enter" className="ms-[105px]" type="text" />
+            <span>
+              <input type="radio" name="" id="" />%
+            </span>
+            <span className="ms-5">
+              {" "}
+              <input type="radio" name="" id="" />$
+            </span>
+          </div>
+        </div>
+        <div className="merit-buttons absolute bottom-0  right-10 pb-10 flex gap-20">
+          <button className="bck-btn">Back</button>
+          <button className="cnt-btn">Continue</button>
+        </div>
+        <div className="budjet">
+          <h1>Budjet</h1>
         </div>
       </div>
-     
     </>
   );
 };
